529043e4bed9cc0039238c7f005ba4d051d5dbf0
quanquan <79596908@qq.com>
2020-10-11 16:31:40 +0800

    增加刀光 水墨shader

--- Assets/8K Skybox Pack Free/Example Scenes/Script/SwordController.cs
+++ Assets/8K Skybox Pack Free/Example Scenes/Script/SwordController.cs
@@ -7,18 +7,25 @@ using UnityEngine;
 public class SwordController : MonoBehaviour
 {
     Animator m_Animator;
+    MeshRenderer meshRenderer;
     System.Random ran;
+
+    public double GetRandomNumber(double minimum, double maximum, int Len=8)   //Len小数点保留位数
+    {
+        return Math.Round(ran.NextDouble() * (maximum - minimum) + minimum, Len);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        System.Random ran = new System.Random();
-        
+        ran = new System.Random();
+        meshRenderer = GetComponent<MeshRenderer>();
         m_Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_Animator.SetFloat("RandomIdle", (float)ran.NextDouble());
+        m_Animator.SetFloat("RandomIdle", (float)GetRandomNumber(0, 1));
     }
 }
